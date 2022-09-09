1dedead3a6665df0efc63cfb376d23d29c79da21
Stefan Stojanovic <stojanovicstefan003@gmail.com>
2022-09-09 10:04:07 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavBar from "./Components/NavBar";
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Calculator from './Components/Calculator';
+import CurrencyConverter from './Components/CurrencyConverter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <BrowserRouter>
+        <NavBar />
+
+        <Routes>
+          <Route path='/' exact element={<Calculator />} />
+          <Route path='/currency-converter' element={<CurrencyConverter />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
