c01ecea955c25f6c6502b7ff33c0a79f9fb941b7
Stefan Stojanovic <stojanovicstefan003@gmail.com>
2022-09-12 18:20:38 +0200

    calculator

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,50 +1,74 @@
 import React, { useState } from "react";
 
-function Calculator(){
+function Calculator() {
 
-    const [result, setResult] = useState("");
+        const [result, setResult] = useState("");
 
-    const buttonClick = (e) => {
-        setResult(result.concat(e.target.name));
-    }
+        const buttonClick = (e) => {
+            
+            // switch(e.target.name){
+            //     case "/": setResult(result + e.target.name); break;
+            //     case "7": setResult(result + e.target.name); break;
+            //     case "8": setResult(result + e.target.name); break;
+            //     case "9": setResult(result + e.target.name); break;
+            //     case "*": setResult(result + e.target.name); break;
+            //     case "4": setResult(result + e.target.name); break;
+            //     case "5": setResult(result + e.target.name); break;
+            //     case "6": setResult(result + e.target.name); break;
+            //     case "+": setResult(result + e.target.name); break;
+            //     case "1": setResult(result + e.target.name); break;
+            //     case "2": setResult(result + e.target.name); break;
+            //     case "3": setResult(result + e.target.name); break;
+            //     case "-": setResult(result + e.target.name); break;
+            //     case "0": setResult(e.target.name); break;
+            //     case ".": setResult(result + e.target.name); break;
+            // }
 
-    const clearButton = () => {
-        setResult("");
-    }
+            setResult(result.toString().concat(e.target.name));
+        }
 
-    const deleteButton = () => {
-        setResult(result.slice(0, -1))
-    }
+        const clearButton = () => {
+            setResult("");
+        }
 
-    const equalButton = () => {
-        setResult(eval(result));
-    }
+        const deleteButton = () => {
+                setResult(result.toString().slice(0, -1));
+        }
+            
 
-    return(
-        <div className="container">
-            <input type="text" value={result}/>
-            <div className="keyboard">
-                <button onClick={clearButton} id="clear-btn">Clear</button>
-                <button onClick={deleteButton}>Del</button>
-                <button onClick={buttonClick} name="/">&divide;</button>
-                <button onClick={buttonClick} name="7">7</button>
-                <button onClick={buttonClick} name="9">8</button>
-                <button onClick={buttonClick} name="9">9</button>
-                <button onClick={buttonClick} name="*">&times;</button>
-                <button onClick={buttonClick} name="4">4</button>
-                <button onClick={buttonClick} name="5">5</button>
-                <button onClick={buttonClick} name="6">6</button>
-                <button onClick={buttonClick} name="+">+</button>
-                <button onClick={buttonClick} name="1">1</button>
-                <button onClick={buttonClick} name="2">2</button>
-                <button onClick={buttonClick} name="3">3</button>
-                <button onClick={buttonClick} name="-">-</button>
-                <button onClick={buttonClick} name="0">0</button>
-                <button onClick={buttonClick} name=".">.</button>
-                <button onClick={equalButton} id="equal-btn">=</button>
+        const equalButton = () => {
+            try{
+                setResult(eval(result));
+            }catch(error){
+                setResult("Error");
+            }
+        }
+
+        return(
+            <div className="container">
+                <div className="output">{result}</div>
+                <div className="keyboard">
+                    <button onClick={clearButton} id="clear-btn">Clear</button>
+                    <button onClick={deleteButton}>Del</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="/">&divide;</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="7">7</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="8">8</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="9">9</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="*">&times;</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="4">4</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="5">5</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="6">6</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="+">+</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="1">1</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="2">2</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="3">3</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="-">-</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name="0">0</button>
+                    <button onClick={buttonClick} onKeyDown={buttonClick} name=".">.</button>
+                    <button onClick={equalButton} id="equal-btn">=</button>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
 export default Calculator;
