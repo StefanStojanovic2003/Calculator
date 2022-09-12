804e0a1de5ffa599fba8bb775a4b65d4f53a0c22
Stefan Stojanovic <stojanovicstefan003@gmail.com>
2022-09-12 21:37:30 +0200

    final

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -25,6 +25,7 @@ function Calculator() {
             // }
 
             setResult(result.toString().concat(e.target.name));
+
         }
 
         const clearButton = () => {
@@ -39,13 +40,27 @@ function Calculator() {
             }
         }
             
+        const decimalButton = (e) => {
+            if(result.includes(".") == true){
+                console.log("Err");
+            }else{
+                setResult(result.toString().concat(e.target.name));
+            }
+        }
 
         const equalButton = () => {
-            try{
-                setResult(eval(result));
-            }catch(error){
-                setResult("Error");
-            }
+            const res = eval(result);
+            
+            if (isFinite(parseFloat(res))) {
+                try{
+                    setResult(eval(result));
+                }catch(error){
+                    setResult("Error");
+                }       
+              }else{
+                return setResult("Error");
+              }
+            
         }
 
         return(
@@ -54,22 +69,22 @@ function Calculator() {
                 <div className="keyboard">
                     <button onClick={clearButton} id="clear-btn">Clear</button>
                     <button onClick={deleteButton}>Del</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="/">&divide;</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="7">7</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="8">8</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="9">9</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="*">&times;</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="4">4</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="5">5</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="6">6</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="+">+</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="1">1</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="2">2</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="3">3</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="-">-</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name="0">0</button>
-                    <button onClick={buttonClick} onKeyDown={buttonClick} name=".">.</button>
-                    <button onClick={equalButton} id="equal-btn">=</button>
+                    <button onClick={buttonClick} name="/">&divide;</button>
+                    <button onClick={buttonClick} name="7">7</button>
+                    <button onClick={buttonClick} name="8">8</button>
+                    <button onClick={buttonClick} name="9">9</button>
+                    <button onClick={buttonClick} name="*">&times;</button>
+                    <button onClick={buttonClick} name="4">4</button>
+                    <button onClick={buttonClick} name="5">5</button>
+                    <button onClick={buttonClick} name="6">6</button>
+                    <button onClick={buttonClick} name="+">+</button>
+                    <button onClick={buttonClick} name="1">1</button>
+                    <button onClick={buttonClick} name="2">2</button>
+                    <button onClick={buttonClick} name="3">3</button>
+                    <button onClick={buttonClick} name="-">-</button>
+                    <button onClick={buttonClick} name="0">0</button>
+                    <button onClick={buttonClick} name=".">.</button>
+                    <button onClick={decimalButton} id="equal-btn">=</button>
                 </div>
             </div>
         )
